4a9aea447ee1b47ad26df1fc5b9c732a334237af
njdeane <njdeane7@gmail.com>
2020-09-26 14:52:11 +1000

    ascii art title added to welcome screen

--- src/welcome.rb
+++ src/welcome.rb
@@ -1,13 +1,25 @@
-puts "Welcome to ZombieRoyale" #this will hopefully be Ascii art
+puts "Welcome to..." #this will hopefully be Ascii art
+    sleep 2.0
+    system("clear")
+
+puts        "#######                                  ######                                   " 
+puts        "     #   ####  #    # #####  # ######    #     #  ####  #   #   ##   #      ######" 
+puts        "    #   #    # ##  ## #    # # #         #     # #    #  # #   #  #  #      #     " 
+puts        "   #    #    # # ## # #####  # #####     ######  #    #   #   #    # #      ##### " 
+puts        "  #     #    # #    # #    # # #         #   #   #    #   #   ###### #      #     " 
+puts        " #      #    # #    # #    # # #         #    #  #    #   #   #    # #      #     " 
+puts        "#######  ####  #    # #####  # ######    #     #  ####    #   #    # ###### ######" 
     sleep 3.0
     system("clear")
-puts "As you are aware the zombie apocalypse has happened... 
-It was fortold in countless TV shows, movies and folklore..."
-    sleep 5.5
+puts "As you are aware the zombie apocalypse has happened..." 
+    sleep 3.0
+    system("clear")
+puts "It was fortold in countless TV shows, movies and folklore..."
+    sleep 3.0
     system("clear")
 puts "Here you will contend for a vaccine so you may walk the earth
 immune to the zombie virus..."
-    sleep 5.0
+    sleep 4.5
     system("clear")
 puts "Do you have what it takes?"
     sleep 2.0
